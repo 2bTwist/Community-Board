505d1c23c40875f38b539c85208bef81f17e3198
2bTwist <ndanjiedmond@gmail.com>
2025-03-01 03:22:23 -0500

    Updated Image links

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,13 +5,13 @@ import "../style/GameBoard.css";
 const games = [
     {
       title: "Chess",
-      image: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Chess.jpg",
+      image: "https://upload.wikimedia.org/wikipedia/commons/4/4e/Chess_pieces_close_up.jpg",
       description: "A classic strategy game of kings, queens, and checkmates.",
       link: "https://www.chess.com/play",
     },
     {
       title: "Monopoly",
-      image: "https://upload.wikimedia.org/wikipedia/en/2/2f/Monopoly_board_on_white_bg.jpg",
+      image: "https://upload.wikimedia.org/wikipedia/commons/4/4a/Monopoly_Boardwalk_%283964604772%29.jpg",
       description: "Build your empire and bankrupt your opponents!",
       link: "https://www.pogo.com/games/monopoly",
     },
@@ -23,45 +23,45 @@ const games = [
     },
     {
       title: "Catan",
-      image: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Catan-Base-Game.jpg",
+      image: "https://upload.wikimedia.org/wikipedia/commons/6/60/Settlers_of_Catan_2016-01-23_17-30-47.jpg",
       description: "Trade, build, and settle in this classic resource-management game.",
       link: "https://catanuniverse.com/en/",
     },
     {
       title: "Risk",
-      image: "https://upload.wikimedia.org/wikipedia/en/3/30/Risk_board_layout.png",
+      image: "https://upload.wikimedia.org/wikipedia/commons/2/28/Partie_de_Risk%2C_%C3%A9dition_1970.jpg",
       description: "Conquer the world in this strategic war game.",
       link: "https://store.steampowered.com/app/1128810/RISK_Global_Domination/",
     },
     {
       title: "Carcassonne",
-      image: "https://upload.wikimedia.org/wikipedia/commons/8/8e/Carcassonne_game.jpg",
+      image: "https://upload.wikimedia.org/wikipedia/commons/d/d4/Americans_in_Asia%3F_%2885080728%29.jpg",
       description: "Build medieval cities and score points in this tile-laying game.",
       link: "https://www.asmodee-digital.com/en/carcassonne/",
     },
     {
       title: "Clue",
-      image: "https://upload.wikimedia.org/wikipedia/en/0/06/Cluedo_1963_US_Edition_Board.jpeg",
+      image: "https://upload.wikimedia.org/wikipedia/commons/1/1f/Hry_a_hlavolamy_2009_-_Cluedo_0173u.jpg",
       description: "Solve the murder mystery with clues and deduction.",
       link: "https://www.hasbro.com/en-us/brands/clue",
     },
     {
       title: "Pandemic",
-      image: "https://upload.wikimedia.org/wikipedia/commons/2/2b/Pandemic_board_game.jpg",
+      image: "https://upload.wikimedia.org/wikipedia/commons/e/e8/Pandemic_Legacy_%2833870877603%29.jpg",
       description: "Work together to stop a global outbreak in this cooperative game.",
       link: "https://www.asmodee-digital.com/en/pandemic/",
     },
     {
       title: "Ticket to Ride",
-      image: "https://upload.wikimedia.org/wikipedia/commons/1/16/Ticket_to_Ride_Board_Game.jpg",
+      image: "https://upload.wikimedia.org/wikipedia/commons/4/41/Ticket_to_Ride_Europe_in_Hattula.jpg",
       description: "Build train routes and connect cities in this fun board game.",
       link: "https://www.asmodee-digital.com/en/ticket-to-ride/",
     },
     {
       title: "Uno",
-      image: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Uno_deck.png",
+      image: "https://upload.wikimedia.org/wikipedia/commons/3/33/Uno%21_%2815694272224%29.jpg",
       description: "A fast-paced card game with wild twists and turns.",
-      link: "https://play.unofreak.com/",
+      link: "https://unoonline.io/",
     }
   ];
 
